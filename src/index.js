4d9f0a61e91fbe506bd0e43f2f7e7b77d1d31ebf
steven <nguyenthanhvo06031999@gmail.com>
2022-11-29 13:17:31 +0700

    CRUD contact

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+import { engine } from 'express-handlebars';
+const app = express();
+const port  = 3000;
+
+app.use(morgan('combined'));
+
+app.engine('handlebars', engine());
+app.engine('view engine', 'handlebars');
+
+app.get('/', (req, res) => res.render('home'));
+
+app.listen(port, () => console.log(`Hello port ${port}`));
